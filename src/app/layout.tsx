8bcf1e8730753f49e9a97b727cedcfb8134c0836
butterfly-react <rdigital4455@gmail.com>
2024-08-29 13:24:12 +0200

    redirect-links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
 					<TanStackProvider>
 
-					<Navbar />
+					{/* <Navbar /> */}
 					{children}
 					</TanStackProvider>
 				</ThemeProvider>
